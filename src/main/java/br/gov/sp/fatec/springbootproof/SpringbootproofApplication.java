6c69a3e2d0bb3ef5c6fe1b1cd7499f02c9b2b407
vitoriasaturnino <vim.saturnino@gmail.com>
2023-10-03 09:25:16 -0300

    build: create a spring project and configure gitpod environment

---
+++ src/main/java/br/gov/sp/fatec/springbootproof/SpringbootproofApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootproof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootproofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootproofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springbootproof/SpringbootproofApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootproof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootproofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
